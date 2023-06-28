7e53af5f7dbce0658448ae17b14a01f59c44b43c
Adwaitk17 <135005825+Adwaitk17@users.noreply.github.com>
2023-06-28 13:04:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Define the target answer for the game
+var levelText = document.getElementById('level');
 var gameover = document.getElementsByClassName('gameover');
 var gameoverScore = document.getElementById('gameover-score');const targetAnswer = 10;
 let score =0;
+let currentLevel = 1;
+levelText.textContent = "Level : "+currentLevel+"/15";
 // Generate random game elements
+
 function generateGameElements() {
   const digits = [];
   const operators = [];
@@ -17,14 +21,34 @@ function generateGameElements() {
 
   return { digits, operators };
 }
+function showGameOverPage() {
+  var currentScore = score; // Replace with the actual current score
+  var scoreDisplay = document.getElementById("current-score");
+  scoreDisplay.textContent = currentScore;
 
+  //var gameOverPage = window.open("", "_blank");
+  var newPageContent='<html><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Menu</title><link href="gameov.css" type="text/css" rel="stylesheet"></head><body><h1>Game Over</h1><div id="score-display">Score:'+ currentScore + '</div><div class="button-container"><button id="start-button"><a href="menu.html">PLAY AGAIN</a></button></body></html>';
+  var newWindow = window.open("", "_self");
+  newWindow.document.open();
+  newWindow.document.write(newPageContent);
+  newWindow.document.close();
+}
 // Get a random operator
 function getRandomOperator( i) {
   const operators = ['+', '-', '*', '/'];
   //const randomIndex = Math.floor(Math.random() * operators.length);
   return operators[i];
 }
+function nextlevel()
+{
+  currentLevel++;
+  document.getElementById('level').innerText = 'Level: ' + currentLevel + " /15";
+  if(currentLevel===15)
+  {
+   location.href= "cong.html";
+  }
 
+}
 // Get the current arrangement
 function getCurrentArrangement() {
   const arrangementElement = document.getElementById('arrangement');
@@ -71,10 +95,15 @@ function handleCorrectAnswer() {
     resultElement.innerText = 'Congratulations! Your answer is correct!';
     score ++;
     updateScore();
+    nextlevel();
     
   } else {
     //resultElement.innerText = 'Sorry, your answer is incorrect. Please try again.';//
-    window.location.href = "gameov.html";
+    //window.location.href = "gameov.html";
+   // hideElements(app);
+   // showElements(gameover);
+    //gameoverScore.textContent = "Your Score is "+score;
+    showGameOverPage();
   }
 
   setTimeout(() => {
@@ -130,10 +159,12 @@ window.addEventListener('DOMContentLoaded', () => {
     const arrangement = getCurrentArrangement();
     if (isCorrect(arrangement, targetAnswer)) {
       setResult('Congratulations! Your arrangement is correct!');
+      
       handleCorrectAnswer();
     } else {
       //setResult('Sorry, your arrangement is incorrect. Please try again.');//
-      window.location.href = "gameov.html";
+     // window.location.href = "gameov.html";
+     showGameOverPage();
     }
   });
 
